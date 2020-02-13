5f993ed9f86faeb42ecaba330a6031f4efb7cf10
g46362 <g46362@WDV20094.resnp.sysnp.shared.fortis>
2020-02-13 13:26:44 +0530

    Added main method to run the application

--- src/main/java/com/bnpp/kata/tennisgame/TennisGame.java
+++ src/main/java/com/bnpp/kata/tennisgame/TennisGame.java
@@ -1,8 +1,11 @@
 package com.bnpp.kata.tennisgame;
 
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class TennisGame {
+
+	private final static Logger logger = Logger.getLogger(TennisGame.class.getName());
 
 	private static final String LOVE = "Love";
 	private static final String FIFTEEN = "Fifteen";
@@ -91,6 +94,28 @@ public class TennisGame {
 
 	String getSecondPlayerName() {
 		return secondPlayerName;
+	}
+
+	public static void main(String args[]) {
+
+		String invalidErrorMessage = "Please enter atleast 4 arguments in the below format \n Player1_Name Player1_Score Player2_Name Player2_Score";
+		if (args.length != 4) {
+			logger.info(invalidErrorMessage);
+		} else {
+
+			TennisGame game = new TennisGame(args[0], args[2]);
+			int firstPlayerScore = Integer.parseInt(args[1]);
+			int secondPlayerScore = Integer.parseInt(args[3]);
+
+			for (int firstPlayerScoreIndex = 0; firstPlayerScoreIndex < firstPlayerScore; firstPlayerScoreIndex++)
+				game.firstPlayerScores();
+
+			for (int secondPlayerScoreIndex = 0; secondPlayerScoreIndex < secondPlayerScore; secondPlayerScoreIndex++)
+				game.secondPlayerScores();
+
+			logger.info(game.getScore());
+		}
+
 	}
 
 }
