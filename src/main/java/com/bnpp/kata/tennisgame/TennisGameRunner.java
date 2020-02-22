e888f61dba7f0ae0e2bb4ef729ed1ad9af3bf8de
aaditya2290 <aaditya_2290@yahoo.co.in>
2020-02-22 21:52:34 +0530

    Refactored: Renamed TennisGameMain class to TennisGamerRunner and extracted constant in main class.

---
+++ src/main/java/com/bnpp/kata/tennisgame/TennisGameRunner.java
@@ -0,0 +1,31 @@
+package com.bnpp.kata.tennisgame;
+
+import java.util.logging.Logger;
+
+public class TennisGameRunner {
+
+	private static final String INVALID_ARGUMENTS_ERROR = "Please enter atleast 4 command line arguments in the format given below: \n FirstPlayerName FirstPlayerScore SecondPlayerName SecondPlayerScore";
+	private final static Logger logger = Logger.getLogger(TennisGame.class
+			.getName());
+
+	public static void main(String args[]) {
+
+		if (args.length != 4) {
+			logger.info(INVALID_ARGUMENTS_ERROR);
+		} else {
+			Player firstPlayer = new Player(args[0]);
+			Player secondPlayer = new Player(args[2]);
+
+			TennisGame game = new TennisGame(firstPlayer, secondPlayer);
+
+			int firstPlayerPoints = Integer.parseInt(args[1]);
+			int secondPlayerPoints = Integer.parseInt(args[3]);
+
+			firstPlayer.scores(firstPlayerPoints);
+			secondPlayer.scores(secondPlayerPoints);
+
+			logger.info(game.getScore());
+		}
+
+	}
+}
